a60b5ca4ab234033eeb81aa3efbce34410bbd2df
Dwip Nandi <143880342+dwip-nandi@users.noreply.github.com>
2024-12-24 19:42:53 +0600

    Update Code

--- B_Intercepted_Inputs.cpp
+++ B_Intercepted_Inputs.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+/*#include <bits/stdc++.h>
 using namespace std;
 int main() {
     int tt;
@@ -17,9 +17,52 @@ int main() {
                 if (cnt[(n - 2) / x] > 0) {
                     cout << x << " " << (n - 2) / x << endl;
                     break;
-                }              
+                }
             }
         }
     }
     return 0;
+}
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        vector<int> hash(n + 1);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+            hash[v[i]]++;
+        }
+        for (int i = 1; i <= n; i++)
+        {
+            if (hash[i] > 0 && ((n - 2) % i == 0))
+            {
+                int com = (n - 2) / i;
+                if (i == com)
+                {
+                    if (hash[com] > 1)
+                    {
+                        cout << i << ' ' << com << endl;
+                        break;
+                    }
+                }
+                else
+                {
+                    if (hash[com] > 0)
+                    {
+                        cout << i << ' ' << com << endl;
+                        break;
+                    }
+                }
+            }
+        }
+    }
 }
