8a49bd6fff703585a142bd4fa8823725be11acaa
Dwip Nandi <143880342+dwip-nandi@users.noreply.github.com>
2025-04-11 23:06:38 +0600

    AATM_Machine

---
+++ ATM_Machine.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        vector<int>v(n);
+        for(int i=0;i<n;i++){
+            cin>>v[i];
+            if(v[i]<=k){
+                cout<<1;
+                k -=v[i];
+            }else cout<<0;
+        }
+        cout<<endl;
+    }
+}
